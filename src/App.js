ae10f09c14334b7652b88a2217d80bbc7fe7854f
sneha-agrawal <snehahts20@gmail.com>
2022-11-19 21:20:54 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <p>
+      <h1>HELLO WORLD</h1>
+    </p>
+  );
+}
+
+export default App;
+
+
+/* class == className
+  no closing tag == inline closing */
